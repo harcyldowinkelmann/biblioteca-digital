1bc42e415eef9a0036cc5b674663722eb23c063a
harcyldoleonardo <134642657+harcyldowinkelmann@users.noreply.github.com>
2025-03-26 20:01:08 -0400

    feat: Inicializando front | Instalando Dependencias

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,34 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
-  <v-dialog max-width="500">
-  <template v-slot:activator="{ props: activatorProps }">
-    <v-btn
-      v-bind="activatorProps"
-      color="surface-variant"
-      text="Open Dialog"
-      variant="flat"
-    ></v-btn>
-  </template>
-
-  <template v-slot:default="{ isActive }">
-    <v-card title="Dialog">
-      <v-card-text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </v-card-text>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-
-        <v-btn
-          text="Close Dialog"
-          @click="isActive.value = false"
-        ></v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
-</v-dialog>
+  <router-view />
 </template>
 
 <style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,40 +1,110 @@
 <template>
-  <v-dialog max-width="500">
-  <template v-slot:activator="{ props: activatorProps }">
-    <v-btn
-      v-bind="activatorProps"
-      color="surface-variant"
-      text="Open Dialog"
-      variant="flat"
-    ></v-btn>
-  </template>
+  <v-card
+    class="mx-auto"
+    max-width="368"
+    color="indigo"
+  >
+    <v-card-item title="Florida">
+      <template v-slot:subtitle>
+        <v-icon
+          class="me-1 pb-1"
+          color="error"
+          icon="mdi-alert"
+          size="18"
+        ></v-icon>
 
-  <template v-slot:default="{ isActive }">
-    <v-card title="Dialog">
-      <v-card-text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </v-card-text>
+        Extreme Weather Alert
+      </template>
+    </v-card-item>
 
-      <v-card-actions>
-        <v-spacer></v-spacer>
+    <v-card-text class="py-0">
+      <v-row align="center" no-gutters>
+        <v-col
+          class="text-h2"
+          cols="6"
+        >
+          64&deg;F
+        </v-col>
 
-        <v-btn
-          text="Close Dialog"
-          @click="isActive.value = false"
-        ></v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
-</v-dialog>
+        <v-col class="text-right" cols="6">
+          <v-icon
+            color="error"
+            icon="mdi-weather-hurricane"
+            size="88"
+          ></v-icon>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <div class="d-flex py-3 justify-space-between">
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-weather-windy"
+      >
+        <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-weather-pouring"
+      >
+        <v-list-item-subtitle>48%</v-list-item-subtitle>
+      </v-list-item>
+    </div>
+
+    <v-expand-transition>
+      <div v-if="expand">
+        <div class="py-2">
+          <v-slider
+            v-model="time"
+            :max="6"
+            :step="1"
+            :ticks="labels"
+            class="mx-4"
+            color="success"
+            density="compact"
+            show-ticks="always"
+            thumb-size="10"
+            hide-details
+          ></v-slider>
+        </div>
+
+        <v-list class="bg-transparent">
+          <v-list-item
+            v-for="item in forecast"
+            :key="item.day"
+            :append-icon="item.icon"
+            :subtitle="item.temp"
+            :title="item.day"
+          >
+          </v-list-item>
+        </v-list>
+      </div>
+    </v-expand-transition>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn
+        :text="!expand ? 'Full Report' : 'Hide Report'"
+        @click="expand = !expand"
+      ></v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script setup>
+  import { ref } from 'vue'
+
+  const labels = { 0: 'SU', 1: 'MO', 2: 'TU', 3: 'WED', 4: 'TH', 5: 'FR', 6: 'SA' }
+  const forecast = [
+    { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\u00B0/12\u00B0' },
+    { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\u00B0/14\u00B0' },
+    { day: 'Thursday', icon: 'mdi-cloud', temp: '25\u00B0/15\u00B0' },
+  ]
+
+  const expand = ref(false)
+  const time = ref(0)
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
